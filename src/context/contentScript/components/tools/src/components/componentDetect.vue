d1de66b681e7c2489777cad38e1a7905e0c8914f
hym19940701 <412947075@qq.com>
2022-11-04 09:31:37 +0800

    Create componentDetect.vue

---
+++ src/context/contentScript/components/tools/src/components/componentDetect.vue
@@ -0,0 +1,25 @@
+<template>
+    <Card class="box-card">
+        <Row>
+            <Col :span="5" :style="{color: state.isDetect ? 'unset' : '#409eff'}">隐藏页面组件结构</Col>
+            <Col :span="3"><Switch v-model:checked="state.isDetect"></Switch></Col>
+            <Col :span="5" :style="{color: state.isDetect ? '#409eff' : 'unset'}">显示页面组件结构</Col>
+        </Row>
+    </Card>
+</template>
+
+<script setup>
+import { Card, Button, Switch, Row, Col } from 'ant-design-vue'
+import { ref, watch } from 'vue'
+import { useHybrid } from '@src/hybrid'
+
+const { state } = useHybrid('componentDetect')
+
+const reset = () => {
+
+}
+
+defineExpose({
+    reset
+})
+</script>
